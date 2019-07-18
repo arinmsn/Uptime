f0f4fdfbe161f55448efb7c974bd97b82d5f0a58
arinmsn <arinmsn@gmail.com>
2019-07-17 17:00:02 -0700

    _checks.post works! When user provides correct *checkObject* parameteres, we got 200 response

--- index.js
+++ index.js
@@ -108,5 +108,6 @@ server.listen(config.port, function(){
 var router = {
     'ping' : handlers.ping,
     'users' : handlers.users,
-    'tokens' : handlers.tokens
+    'tokens' : handlers.tokens,
+    'checks' : handlers.checks
 };
